ef52e00759c7bf47a1137705ad28da50fc3bfb30
Mr. Mergjoto <mr.mergjoto@gmail.com>
2017-09-27 21:14:24 -0400

    simple code line changes

--- merge_test_one/example.js
+++ merge_test_one/example.js
@@ -1,8 +1,8 @@
 
 var exampleFunction = function() {
   var line_one = 1;
-  var line_two = 2;
+  var line_two = "two";
   var line_three = 3;
-  var line_four = "four";
+  var line_four = 4;
   var line_five = "five";
 }

cf0af692a6f9aaadc5b400712ff721e7d2f9337d
Mr. Mergjoto <mr.mergjoto@gmail.com>
2017-09-27 20:55:34 -0400

    test start

--- merge_test_one/description.md
+++ merge_test_one/description.md
@@ -1,2 +1,6 @@
+
 ## Merge Test One Description
 
+1. Create "example.js" to local hard drive
+0. Add a JavaScript method and a bunch of lines
+0. 
---
+++ merge_test_one/example.js
@@ -0,0 +1,8 @@
+
+var exampleFunction = function() {
+  var line_one = 1;
+  var line_two = 2;
+  var line_three = 3;
+  var line_four = "four";
+  var line_five = "five";
+}
